7051957bf894c97d057001dacfa933354d3ca36d
Emiliya Chavdarova <emiliya@carrypicker.com>
2022-03-21 15:38:28 +0100

    Auto-generated commit [ci skip]

---
+++ assets/files/loactions.ts
@@ -0,0 +1,47 @@
+export const locationList = [
+    {
+        name: 'Neuss',
+        lat: 51.18157877453713,
+        lng: 6.679805251495432,
+    },
+    {
+        name: 'Hamburg',
+        lat: 53.5511,
+        lng: 9.9937,
+    },
+    {
+        name: 'Berlin',
+        lat: 52.5200,
+        lng: 13.4050,
+    },
+    {
+        name: 'Bremen',
+        lat: 53.073635,
+        lng: 8.806422,
+    },
+    {
+        name: 'München',
+        lat: 48.137154,
+        lng: 11.576124,
+    },
+    {
+        name: 'Barcelona',
+        lat: 41.3874,
+        lng: 2.1686,
+    },
+    {
+        name: 'Milan',
+        lat: 45.4642,
+        lng: 9.1900,
+    },
+    {
+        name: 'Verona',
+        lat: 45.4384,
+        lng: 10.9916,
+    },
+    {
+        name: 'Wien',
+        lat: 48.2082,
+        lng: 16.3738,
+    }
+]
