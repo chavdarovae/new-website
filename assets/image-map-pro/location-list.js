980e126e51c4b09af9dbd3646f5969abaa8691d7
Emiliya Chavdarova <emiliya@carrypicker.com>
2022-04-05 13:45:16 +0200

    Auto-generated commit [ci skip]

--- assets/image-map-pro/location-list.js
+++ assets/image-map-pro/location-list.js
@@ -49,14 +49,14 @@ locations.forEach(loc => {
         "id": "text-" + (4601 + loc.id),
         "title": loc.name + "Name",
         "type": "text",
-        "x": mobileVersion ? loc.x - 0.2 :loc.x - 0.1,
+        "x": mobileVersion ? loc.x - 0.2 :loc.x - 0.3,
         "y": loc.y + 1.2,
-        "x_image_background": mobileVersion ? loc.x - 0.2 :loc.x - 0.1,
+        "x_image_background": mobileVersion ? loc.x - 0.2 :loc.x - 0.3,
         "y_image_background": loc.y + 1.2,
         "text": {
             "text": loc.name,
             "font_family": "Roboto",
-            "font_size": mobileVersion ? 5 : 7
+            "font_size": mobileVersion ? 6 : 9
         },
         "tooltip_content": {
             "squares_settings": {
