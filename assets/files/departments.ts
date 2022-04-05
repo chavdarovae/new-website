1ee2e66277e7b1811c61e234754d36c1b44019ae
Emiliya Chavdarova <emiliya@carrypicker.com>
2022-04-05 12:25:00 +0200

    Auto-generated commit [ci skip]

---
+++ assets/files/departments.ts
@@ -0,0 +1,140 @@
+Abrechnung
+Abrechnung / Lademittel 
+Abrechnung / Schäden 
+Abrechnung / Service international 
+Assistenz der Geschäftsführung
+Auszubildende 
+Baden Württemberg / Saarland 
+Baden-Württemberg 
+Baden-Württemberg / Allgäu 
+Baden-Württemberg / Bayern
+Baden-Württemberg / F-Elsass 
+Baden-Württemberg / Hessen 
+Baden-Württemberg / Saarland 
+Balkan / Italien / Ungarn / Rumänien / Bulgarien / Griechenland / Albanien 
+Bayern
+Bayern / Bodensee 
+Bayern / Nahverkehr 
+Bayern / Süddeutschland 
+Belgien / Niederlande 
+BeNeLux 
+Benelux / England / Österreich / Slowakei 
+Benelux / Österreich 
+BeNeLux / Spanien / Portugal / Frankreich 
+Berlin / Brandenburg / Magdeburg / Meck-Pom / Sachsen / Sachsen-Anhalt / Thüringen 
+Berlin / Magdeburg
+Berlin / Meck-Pom
+Berlin / Mecklenburg
+Berlin / Mecklenburg / Hannover 
+Berlin / Mecklenburg-Vorpommern 
+Berlin / Osten 
+Berlin / Sachsen-Anhalt 
+Bremen / Ems / Hannover 
+Bremen / Friesland 
+Bremen / Hannover
+Bremen / Niedersachsen 
+Bremen / Ostfriesland / Münsterland 
+Bremen / Rheinland / Ostfriesland 
+Buchhaltungsleitung
+Buchstaben A - G
+Buchstaben H - M
+Buchstaben N - S
+Buchstaben T - Z
+Chemnitz / Leipzig 
+Dresden 
+Dresden / Berlin / Mecklenburg-Vorpommern 
+FFM / Mannheim / Saar 
+Frankfurt / Fulda 
+Frankfurt / Koblenz / Gießen / Saarland / Frankreich / Luxemburg 
+Frankfurt / Würzburg / Mannheim / Saarland / Gießen 
+Frankreich 
+GB / CH / I / E / F / Skandinavien / Türkei 
+Geschäftsleitung
+Halle / Leipzig /Chemnitz / Dresden
+Hamburg
+Hamburg / Bremen 
+Hamburg / Bremen / Niedersachsen 
+Hamburg / Schleswig-Holstein 
+Hamburg / Schleswig-Holstein / Bremen / Bremerhaven 
+Hamburg / Schwerin / Schleswig-Holstein 
+Hannnover / Kassel 
+Hannover / Bielefeld 
+Hannover / Kassel 
+Hannover / Kassel / Bielefeld / Braunschweig / Rhein / Ruhr 
+Hessen 
+Hessen / Saarland 
+Hessen / Saarland / Baden-Württemberg 
+Hessen /Rheinland-Pfalz / Saarland 
+Italien / Schweiz 
+Karlsruhe / Mannheim / Saar / FFM
+Lademiitel 
+Lademittel / Abrechnund / Schäden 
+Lademittel / Schäden 
+Lademittel Unternehmer 
+Luxemburg 
+Meck-Pom / Berlin / Brandenburg 
+Meck-Pom / Hamburg 
+Mecklenburg-Vorpommern / Schleswig-Holstein
+Messetransporte / Schweiz 
+Nahverkehr
+Nahverkehr / Luxemburg / Saarland / Express 
+Nahverkehr / Sonderfahrten / Schleswig-Holstein 
+nationale und internationale temperaturgeführte Transporte 
+Neufra Italien / Österreich / Ungarn
+Neufra Polen
+Neufra Spanien
+Niedersachsen 
+Nord 
+Norden / Meck-Pom / Nahverkehr 
+Nürnberg / München 
+Nürnberg / Regensburg / Augsburg / München
+Nürnberg / Stuttgart / Bodensee 
+Osnabrück / Münster / Ostwestfalen / Kassel / Hannover 
+Ost 
+Ostdeutschland 
+Osten 
+Österreich 
+Österreich / CZ 
+Österreich / Italien / Schweiz
+Österreich / Schweiz 
+Österreich PLZ 1, 2, 3, 7, 8 
+Österreich PLZ 4, 5, 6, 9
+Osteuropa
+Osteuropa / Express / Italien / Österreich / Temperaturgeführte Transporte 
+Ostwestf. / Niedersachsen 
+Personalabteilung
+Polen 
+Polen / Baltikum / Großbrítannien 
+Polen / CZ / Ungarn / Balkan
+Polen / Tschechien / Spanien / Frankreich / Slowakei 
+Recherche / Service 
+Rhein 
+Rhein / Ruhr 
+Rhein / Ruhr / Belgien / Niederlande 
+Rhein / Ruhr / Bielefeld / Osnabrück 
+Rhein / Ruhr / Niederlansde 
+Rhein / Ruhr / Siegerland 
+Rhein-Ruhr 
+Rheinland / Siegerland / Mosel / Hamburg / Bremen / Ostwestfalen / Nahverkehr 
+Rheinland-Pfalz / Nordrhein-Westfalen 
+Saarland / Mannheim / Frankfurt 
+Saarland / Rheinland-Pfalz 
+Sachsen / Sachsen-Anhalt / Thüringen 
+Schäden
+Schäden / Abrechnung 
+Schäden / Lademittel Kunden 
+Schäden & Assistenz der Geschäftsleitung
+Schleswig-Holstein 
+Schweiz / Italien / Frankreich / BeNeLux
+Spanien / Portugal / Frankreich 
+Stuttgart / Freiburg / Würzburg / Nürnberg 
+Stuttgart / Ulm / Freiburg
+Süd-Osten / Berlin / Harz / Magdeburg 
+Thüringen / Nordbayern / Kassel
+Thüringen / Sachsen
+Thüringen / Sachsen / Sachsen-Anhalt 
+Türkei / Spezialverkehre / Dänemark / Nordeuropa 
+Türkei / Spezialverkehre / Vorderasien 
+Vertretungen
+West 
+West- und Nordeuropa
